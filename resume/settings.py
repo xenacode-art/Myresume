4d0d99c84bea1a9719e0a701b45e1fa10dc9b37e
xena <kaja.obinna@lmu.edu.ng>
2021-08-20 10:26:41 +0100

    updated file structures

--- resume/settings.py
+++ resume/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%)qu-9d8j*s$qejll(ytr24$9v^r_qvkq2cxlsq&f00u@$gp)'
+SECRET_KEY =( 'django-insecure-&%)qu-9d8j*s$qejll(ytr24$9v^r_qvkq2cxlsq&f00u@$gp)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast = bool)
 
 ALLOWED_HOSTS = []
 
@@ -137,4 +137,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-ALLOWED_HOSTS = 'https://glacial-cliffs-77995.herokuapp.com/'
+# ALLOWED_HOSTS = 'https://glacial-cliffs-77995.herokuapp.com/'
